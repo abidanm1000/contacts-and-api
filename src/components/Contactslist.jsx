846f1ff8a7bf3de0b90d997f34d0cbd8b2460d8d
Abidan Martinez <abidanmtz@gmail.com>
2022-02-27 20:31:43 -0800

    adding counter

--- src/components/Contactslist.jsx
+++ src/components/Contactslist.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Contact from './Contact'
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
 
-function Contactslist(props) {
+function Contactslist({ list }) {
     // console.log(props.list)
 
     // let title = useRef('friends')
     // console.log(title.current.innerText)
+    let defaultImage = 'https://e7.pngegg.com/pngimages/889/832/png-clipart-google-contacts-mobile-app-contact-manager-app-store-android-application-package-email-miscellaneous-blue.png';
 
-    const [image, setImage] = useState('https://e7.pngegg.com/pngimages/889/832/png-clipart-google-contacts-mobile-app-contact-manager-app-store-android-application-package-email-miscellaneous-blue.png');
+    const [image, setImage] = useState(defaultImage);
     useEffect(()=> {
         fetch('https://jsonplaceholder.typicode.com/photos/?_limit=10')
         .then(response => response.json())
@@ -17,14 +18,12 @@ function Contactslist(props) {
       }, [])
 
 
-    // console.log(image[0].url)
-
     return (
         <div className='Contactslist'>
             <div className="title">
                 <h1>Contacts</h1>
-                {/*{props.list.map(item => <Contact name={item.name} number={item.phone}/>)}*/}
-                {props.list.map((item, index) => <Contact key={item.id} name={item.name} number={item.phone} image={image[index].url || 'https://e7.pngegg.com/pngimages/889/832/png-clipart-google-contacts-mobile-app-contact-manager-app-store-android-application-package-email-miscellaneous-blue.png'}/>)}
+                <h2 id='counter'><img id='contacts-image' src={defaultImage}/>{list.length}</h2>
+                {list.map((item, index) => <Contact key={item.id} name={item.name} number={item.phone} image={image[index].url || defaultImage}/>)}
             </div>
         </div>
     )
